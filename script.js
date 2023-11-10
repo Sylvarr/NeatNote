d9fa96eedd04048fc622eae1e8aa6a4922f7bd2d
Whitessx <whitessx@gmail.com>
2023-11-10 21:46:47 +0100

    Added a delete button for each note
    next step is to make it functional

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const newNoteButton = document.querySelector(".controls_new_note");
+const newNoteButton = document.getElementById("new_note_button");
+const deleteNoteButton = document.getElementById("delete_note_button");
 const notesContainer = document.querySelector(".notes_container");
 const modal = document.querySelector(".modal");
 const invisiblePanel = document.querySelector(".invisible_panel");
@@ -11,6 +12,7 @@ const openedNotes = document.querySelector(".opened_notes ");
 const openedNotesTitle = document.querySelector(".opened_notes_note_title");
 const openedNotesContent = document.querySelector(".opened_notes_note_content");
 const openedNotesDate = document.querySelector(".opened_notes_note_date");
+const deleteButton = document.getElementById("delete_note_button");
 
 let noteArray = [];
 
@@ -39,15 +41,31 @@ function createNote(title, message, date) {
   noteContainer.classList.add("notes_container_note");
   notesContainer.appendChild(noteContainer);
 
+  let noteBundle = document.createElement("div");
+  noteBundle.classList.add("notes_container_bundle");
+  noteContainer.appendChild(noteBundle);
+
   noteContainer.addEventListener("click", () => {
     openNote(title, message, date);
     notesVisible();
   });
 
   let noteTitle = document.createElement("h2");
-  noteTitle.className = "notes_container_note_title";
-  noteContainer.appendChild(noteTitle);
+  noteTitle.classList.add("notes_container_note_title");
+  noteBundle.appendChild(noteTitle);
   noteTitle.textContent = title;
+
+  let closeButton = document.createElement("span");
+  closeButton.classList.add("notes_container_close");
+  noteBundle.appendChild(closeButton);
+  closeButton.textContent = "X";
+  closeButton.addEventListener("click", (e) => {
+    e.stopPropagation();
+    noteContainer.remove();
+  });
+
+  let deleteNoteButton = document.createElement("span");
+  deleteNoteButton.className = "notes_container_close";
 
   let noteMessage = document.createElement("p");
   noteMessage.classList.add("notes_container_note_body");
@@ -58,6 +76,14 @@ function createNote(title, message, date) {
   noteDate.classList.add("notes_container_note_date");
   noteContainer.appendChild(noteDate);
   noteDate.textContent = date;
+}
+
+function toggleCloseButtons() {
+  document
+    .querySelectorAll(".notes_container_close")
+    .forEach(function (closeButton) {
+      closeButton.classList.toggle("visible");
+    });
 }
 
 function openNote(title, message, date) {
@@ -86,21 +112,17 @@ function loadNotes() {
 
 document.addEventListener("DOMContentLoaded", loadNotes);
 
-newNoteButton.addEventListener("click", () => {
-  toggleVisible();
-});
+newNoteButton.addEventListener("click", toggleVisible);
+
+deleteButton.addEventListener("click", toggleCloseButtons);
 
 modal.addEventListener("click", (e) => {
   e.stopPropagation();
 });
 
-invisiblePanel.addEventListener("click", () => {
-  toggleVisible();
-});
+invisiblePanel.addEventListener("click", toggleVisible);
 
-cancelButton.addEventListener("click", (e) => {
-  toggleVisible();
-});
+cancelButton.addEventListener("click", toggleVisible);
 
 saveButton.addEventListener("click", (e) => {
   e.preventDefault();
